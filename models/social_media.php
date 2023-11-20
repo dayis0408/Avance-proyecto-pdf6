70cce2e32e98916bc43ad2ce32f240c7238ef0e8
dayis0508 <dayana.08.2005@gmail.com>
2023-11-19 21:01:58 -0500

    subiendo proyecto

---
+++ models/social_media.php
@@ -0,0 +1,62 @@
+<?php
+    class SocialMedial extends Conectar{
+
+        public function get_socialMedia(){
+            $social = parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM social_media WHERE est=1";
+            $sql=$social->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function get_socialMedia_con_parametro($socmed_id){
+            $social = parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM social_media WHERE socmed_id=?";
+            $sql=$social->prepare($sql);
+            $sql->bindValue(1,$socmed_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function insert_socialMedia($socmed_icono,$socmed_url){
+            $social = parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO social_media (socmed_id,socmed_icono,socmed_url,est) VALUES(NULL,?,?,1)";
+            $sql=$social->prepare($sql);
+            $sql->bindValue(1,$socmed_id);
+            $sql->bindValue(2,$socmed_url);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function update_socialMedia($socmed_id,$socmed_icono,$socmed_url){
+            $social = parent::conexion();
+            parent::set_names();
+            $sql="UPDATE usuarios
+                    SET
+                        socmed_icono = ?,
+                        socmed_url = ?
+                    WHERE
+                        socmed_id = ?";
+            $sql=$social->prepare($sql);
+            $sql->bindValue(1,$socmed_icono);
+            $sql->bindValue(2,$socmed_url);
+            $sql->bindValue(3,$socmed_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+
+        }
+
+        public function delete_socialMedia($socmed_id){
+            $social = parent::conexion();
+            parent::set_names();
+            $sql="UPDATE usuarios SET est = 0 WHERE socmed_id=?";
+            $sql=$social->prepare($sql);
+            $sql->bindValue(1,$socmed_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+    }
+?>
